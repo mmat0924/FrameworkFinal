e2f4e0de7b364f3c2785b81038b0ee0fab0f604f
melvin <mel.matt0708@outlook.com>
2020-12-08 13:24:41 -0600

    created base class with initialize driver method

---
+++ E2E/src/main/java/privatefw/E2E/App.java
@@ -0,0 +1,13 @@
+package privatefw.E2E;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
