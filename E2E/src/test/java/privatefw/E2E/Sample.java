f14647b7d87ff1ab972c351394ed29e2f296cc6d
melvin <mel.matt0708@outlook.com>
2020-12-09 17:39:44 -0600

    integrated testng log4j and listeners

---
+++ E2E/src/test/java/privatefw/E2E/Sample.java
@@ -0,0 +1,15 @@
+package privatefw.E2E;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+
+	@Test
+	public void sample() {
+		
+		System.out.println("Hey, I'm just executing for fun!");
+	}
+	
+	
+}
